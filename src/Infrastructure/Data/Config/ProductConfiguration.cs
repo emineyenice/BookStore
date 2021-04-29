009d8c4ad9b11be369a81000534ec1a1d256da21
Emine YENİCE <emine_yenice@hotmail.com>
2021-04-29 09:24:29 +0300

    Entities

---
+++ src/Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Config
+{
+    public class ProductConfiguration: IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.HasOne(x => x.Author)
+                .WithMany()
+                .HasForeignKey(x => x.AuthorId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(x => x.Category)
+                .WithMany()
+                .HasForeignKey(x => x.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+            //Ürünler-Yazar 1-1 ilişki => ürün silindiğinde yazarı da sil
+            //Ürünler-Kategori 1-1 ilişki=> ürün silindiğinde kategoriyi silme
+
+        }
+    }
+}
