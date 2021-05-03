c2b466946a63765a77893b625cddc4ba0fb6c0ac
Emine YENİCE <emine_yenice@hotmail.com>
2021-05-03 12:25:31 +0300

    HomeViewModelService

---
+++ src/ApplicationCore/Specifications/ProductsWithAuthourSpecification.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationCore.Entities;
+using Ardalis.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Specifications
+{
+    public class ProductsWithAuthourSpecification:Specification<Product>
+    {
+        public ProductsWithAuthourSpecification()
+        {
+            Query.Include(x => x.Author);
+        }
+    }
+}
